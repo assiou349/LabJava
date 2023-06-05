f90bfb54c357372fb518f963fc3f3e5412c2abc5
Assiou KARABOU <karaboutchilabalo@yahoo.fr>
2023-06-05 15:52:15 -0400

    Building projet skeleton

---
+++ TechnicalAdvisorService/src/test/java/com/labjava/skillguest/technicaladvisorservice/TechnicalAdvisorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labjava.skillguest.technicaladvisorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnicalAdvisorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/main/java/com/labjava/skillguest/LabJavaSkillGestApplication.java
@@ -0,0 +1,13 @@
+package com.labjava.skillguest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabJavaSkillGestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabJavaSkillGestApplication.class, args);
+    }
+
+}
